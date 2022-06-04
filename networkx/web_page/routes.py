432483495680d7a44bddc61cfeeb0b9d6e89d28e
neta-r <neta2611@walla.com>
2022-06-04 22:54:42 +0300

    redirected successfully

--- networkx/web_page/routes.py
+++ networkx/web_page/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, url_for, redirect
+from flask import render_template, flash, url_for, redirect, request
 
 from networkx.web_page import app
 from networkx.web_page.templates.forms import Parameters
@@ -12,8 +12,7 @@ def result():
 @app.route("/", methods=['GET', 'POST'])
 def home():
     form = Parameters()
-    if not form.validate_on_submit():
-        return render_template('homepage.html', form=form)
+    if request.method == 'POST':
+        return redirect(url_for('result'))
     else:
-        flash(f'Welcome, !', 'success')
-        return redirect(url_for(result.__name__))
+        return render_template('homepage.html', form=form)
